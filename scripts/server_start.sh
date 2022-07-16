3090fad00d9b5b6f923659a28b61f3cc1783306b
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-07-16 16:33:29 -0300

    Revert to older approach to startup

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8080 look-at-this-car-0.0.1-SNAPSHOT.jar
+sudo java -jar -Dserver.port=8080 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
