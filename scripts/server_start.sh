b4fce69518e8e95a4154282d72dc1364a47bd047
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-07-16 14:48:31 -0300

    Fix execute application command

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8080 look-at-this-car-0.0.1-SNAPSHOT.jar
+java -jar -Dserver.port=8080 look-at-this-car-0.0.1-SNAPSHOT.jar
